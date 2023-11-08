d66af56629de6495702f14d8fb223b0713dfe27d
HaithamBH <haitham.benhammouda@gmail.com>
2023-11-08 23:18:41 +0100

    tailwind config

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,21 +4,7 @@ import daisyui from 'daisyui';
 export default {
   content: ['./index.html', './src/**/*.{html,tsx}'],
   daisyui: {
-    themes: [
-      {
-        mytheme: {
-          primary: '#14213d',
-          secondary: '#fca311',
-          accent: '#e5e5e5',
-          neutral: '#2a323c',
-          'base-100': '#4b5563',
-          info: '#3abff8',
-          success: '#36d399',
-          warning: '#fbbd23',
-          error: '#f87272'
-        }
-      }
-    ]
+    themes: ['night']
   },
   plugins: [daisyui]
 };

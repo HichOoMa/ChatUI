c64d24905b7125a97bc7f3b9933fbf837a4cb97f
HaithamBH <haitham.benhammouda@gmail.com>
2023-11-09 21:27:12 +0100

    sidebar component

---
+++ src/components/sidebar.tsx
@@ -0,0 +1,16 @@
+import Friend from './friend';
+
+const Sidebar = () => {
+  return (
+    <div className="h-[calc(100vh-4rem)] w-60 fixed bg-base-200">
+      <Friend
+        name={'Haithem BenHammouda'}
+        lastMsg={'test last message aaaaaaa'}
+        profilePhoto={'https://www.shareicon.net/data/256x256/2016/05/26/771188_man_512x512.png'}
+        isOnline={false}
+      />
+    </div>
+  );
+};
+
+export default Sidebar;

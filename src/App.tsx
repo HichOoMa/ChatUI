4bc80514e51d235325afabd485c67c2ed0e5c2cd
HaithamBH <haitham.benhammouda@gmail.com>
2023-11-12 23:27:29 +0100

    Add login and chat routes to App component

--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,22 @@
 import { Route, Routes } from 'react-router-dom';
 import Register from './pages/register';
 import CenterLayout from './layout/center.layout';
+import MainLayout from './layout/main.layout';
+import Messages from './pages/messages';
 
 function App() {
   return (
     <>
       <Routes>
-        <Route path="/" element={<CenterLayout />}>
+        <Route path="/register" element={<CenterLayout />}>
           <Route path="/register" element={<Register />} />
           // add login route
         </Route>
+        <Route path="/" element={<MainLayout />}>
+          <Route path="/chat" element={<Messages />} />
+        </Route>
+        <Route path="*" element={<h1>404</h1>} />
       </Routes>
-      {/* <Routes>
-          <MainLayout>
-            <Route path="/" element={<h1>Main Layout</h1>} />
-          </MainLayout>
-        </Routes> */}
     </>
   );
 }

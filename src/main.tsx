e9c3b3b2b87e2dc44b624ca79152c28a71e44e8e
HaithamBH <haitham.benhammouda@gmail.com>
2023-11-12 23:26:55 +0100

    Add dotenv configuration to main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -1,8 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
+import dotenv from 'dotenv';
 import App from './App.tsx';
 import './index.css';
+
+dotenv.config();
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>

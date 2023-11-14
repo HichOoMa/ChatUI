d04cfed4dc606f0e95c1c36c6385950242fc6c1a
HaithamBH <haitham.benhammouda@gmail.com>
2023-11-14 22:02:44 +0100

    Add WebSocket connection buttons to Messages page

---
+++ src/pages/messages.tsx
@@ -0,0 +1,41 @@
+import { connectSocket, disconnectSocket } from '../websocket/init';
+
+const Messages = () => {
+  return (
+    <>
+      <button onClick={connectSocket} className="w-[600px] bg-primary">
+        Connect
+      </button>
+      <button onClick={disconnectSocket} className="w-[600px] bg-primary">
+        Disconnect
+      </button>
+      {/* <div className="chat chat-start">
+        <div className="chat-image avatar">
+          <div className="w-10 rounded-full">
+            <img src="/images/stock/photo-1534528741775-53994a69daeb.jpg" />
+          </div>
+        </div>
+        <div className="chat-header">
+          Obi-Wan Kenobi
+          <time className="text-xs opacity-50">12:45</time>
+        </div>
+        <div className="chat-bubble">You were the Chosen One!</div>
+        <div className="chat-footer opacity-50">Delivered</div>
+      </div>
+      <div className="chat chat-end">
+        <div className="chat-image avatar">
+          <div className="w-10 rounded-full">
+            <img src="/images/stock/photo-1534528741775-53994a69daeb.jpg" />
+          </div>
+        </div>
+        <div className="chat-header">
+          Anakin
+          <time className="text-xs opacity-50">12:46</time>
+        </div>
+        <div className="chat-bubble">I hate you!</div>
+        <div className="chat-footer opacity-50">Seen at 12:46</div>
+      </div> */}
+    </>
+  );
+};
+export default Messages;

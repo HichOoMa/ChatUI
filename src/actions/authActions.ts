a755e2e858a6e56c8002d4492be1cbb2452e1ac1
HaithamBH <haitham.benhammouda@gmail.com>
2023-11-14 22:02:24 +0100

    Add user login functionality and redirect to chat page upon successful registration

--- src/actions/authActions.ts
+++ src/actions/authActions.ts
@@ -1,18 +1,38 @@
+/* eslint-disable react-hooks/rules-of-hooks */
 /* eslint-disable @typescript-eslint/no-explicit-any */
+import { useNavigate } from 'react-router-dom';
+import { useAuth } from '../store/init';
 import api from './api';
 
 export function UserRegister(username: string, email: string, password: string) {
+  const updateAuth = useAuth((state) => state.updateAuth);
+  const navigate = useNavigate();
+  console.log('test 23');
   api
     .POST('/register', {
       body: { username, email, password }
     })
     .then((data: any) => {
       localStorage.setItem('token', data.token);
-      window.location.href = '/';
+      updateAuth(true);
+      navigate('/chat');
     })
     .catch((error) => {
       // catch error
       // add notification
       console.log(error);
+    });
+}
+
+export function login(username: string, password: string) {
+  const updateAuth = useAuth((state) => state.updateAuth);
+  api
+    .POST('/login', {
+      body: { username, password }
+    })
+    .then((data: any) => {
+      localStorage.setItem('token', data.token);
+      updateAuth(true);
+      window.location.href = '/';
     });
 }

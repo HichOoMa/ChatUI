ddc6726320b40c2d74516d14157484c55753421d
HaithamBH <haitham.benhammouda@gmail.com>
2023-11-25 15:50:51 +0100

    Update navbar background color

--- src/components/navbar.tsx
+++ src/components/navbar.tsx
@@ -2,7 +2,7 @@ import NotificationsIcon from '@mui/icons-material/Notifications';
 
 function Navbar() {
   return (
-    <div className="navbar bg-base-200 shadow-lg">
+    <div className="navbar bg-base-100 shadow-lg">
       <div className="flex-1">
         <a className="btn btn-ghost normal-case text-xl">daisyUI</a>
       </div>

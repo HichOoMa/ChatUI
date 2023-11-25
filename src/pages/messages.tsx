438868bc4a01ef63bfdf01f036759cfe5a0e7cdc
HaithamBH <haitham.benhammouda@gmail.com>
2023-11-25 15:51:44 +0100

    Refactor messages page and components

--- src/pages/messages.tsx
+++ src/pages/messages.tsx
@@ -1,41 +1,28 @@
-import { connectSocket, disconnectSocket } from '../websocket/init';
+import Message from '../components/message';
+import SendField from '../components/sendField';
 
 const Messages = () => {
+  const d = new Date();
   return (
-    <>
-      <button onClick={connectSocket} className="w-[600px] bg-primary">
-        Connect
-      </button>
-      <button onClick={disconnectSocket} className="w-[600px] bg-primary">
-        Disconnect
-      </button>
-      {/* <div className="chat chat-start">
-        <div className="chat-image avatar">
-          <div className="w-10 rounded-full">
-            <img src="/images/stock/photo-1534528741775-53994a69daeb.jpg" />
-          </div>
-        </div>
-        <div className="chat-header">
-          Obi-Wan Kenobi
-          <time className="text-xs opacity-50">12:45</time>
-        </div>
-        <div className="chat-bubble">You were the Chosen One!</div>
-        <div className="chat-footer opacity-50">Delivered</div>
+    <main className="h-full w-full relative">
+      <div className="p-4">
+        <Message
+          username={'Hamed'}
+          userphotoUrl={'https://www.shareicon.net/data/512x512/2016/09/15/829452_user_512x512.png'}
+          content={'ooooooooo allaowska test test test'}
+          date={d}
+          showDetails={false}
+        />
+        <Message
+          username={'Hamed'}
+          userphotoUrl={'https://www.shareicon.net/data/512x512/2016/09/15/829452_user_512x512.png'}
+          content={'ooooooooo allaowska test test test'}
+          date={d}
+          showDetails={true}
+        />
       </div>
-      <div className="chat chat-end">
-        <div className="chat-image avatar">
-          <div className="w-10 rounded-full">
-            <img src="/images/stock/photo-1534528741775-53994a69daeb.jpg" />
-          </div>
-        </div>
-        <div className="chat-header">
-          Anakin
-          <time className="text-xs opacity-50">12:46</time>
-        </div>
-        <div className="chat-bubble">I hate you!</div>
-        <div className="chat-footer opacity-50">Seen at 12:46</div>
-      </div> */}
-    </>
+      <SendField />
+    </main>
   );
 };
 export default Messages;

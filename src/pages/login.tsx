be5569ba34d2d82c0d1310b0576b02ee3d5919cd
HaithamBH <haitham.benhammouda@gmail.com>
2023-12-21 22:04:16 +0100

    login page

--- src/pages/login.tsx
+++ src/pages/login.tsx
@@ -1,7 +1,27 @@
-import { useAuthStore } from '../store/auth';
+import { ChangeEvent, useState } from "react";
+import { useAuthStore } from "../store/auth";
 
 export default function Login() {
-  const { test } = useAuthStore();
+  const { login } = useAuthStore();
+  interface ILoginForm {
+    email: string;
+    password: string;
+  }
+  const [loginForm, setLoginForm] = useState<ILoginForm>({
+    email: "",
+    password: "",
+  });
+
+  const onFieldChange = (e: ChangeEvent<HTMLInputElement>) => {
+    setLoginForm((state) => ({
+      ...state,
+      [e.target.name]: e.target.value,
+    }));
+  };
+
+  const loginSubmit = () => {
+    login(loginForm.email, loginForm.password);
+  };
 
   return (
     <div className="flex flex-col justify-center px-6 py-12 lg:px-12 lg:py-16 lg:bg-base-200 lg:rounded-lg">
@@ -17,25 +37,32 @@ export default function Login() {
       </div>
 
       <div className="mt-10 sm:mx-auto sm:w-full sm:max-w-sm">
-        <form className="space-y-6" action="#" method="POST">
+        <form className="space-y-6">
           <div>
-            <label className="block text-sm font-medium leading-6 ">Email address</label>
+            <label className="block text-sm font-medium leading-6 ">
+              Email address
+            </label>
             <div className="mt-2">
               <input
                 id="email"
                 name="email"
                 type="email"
+                onChange={() => onFieldChange}
                 required
                 className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
               />
             </div>
           </div>
-
           <div>
             <div className="flex items-center justify-between">
-              <label className="block text-sm font-medium leading-6">Password</label>
+              <label className="block text-sm font-medium leading-6">
+                Password
+              </label>
               <div className="text-sm">
-                <a href="#" className="font-semibold text-indigo-600 hover:text-indigo-500">
+                <a
+                  href="#"
+                  className="font-semibold text-indigo-600 hover:text-indigo-500"
+                >
                   Forgot password?
                 </a>
               </div>
@@ -45,6 +72,7 @@ export default function Login() {
                 id="password"
                 name="password"
                 type="password"
+                onChange={() => onFieldChange}
                 required
                 className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
               />
@@ -54,8 +82,9 @@ export default function Login() {
           <div>
             <button
               type="submit"
-              onClick={() => test()}
-              className="flex w-full justify-center rounded-md bg-indigo-600 px-3 py-1.5 text-sm font-semibold leading-6 text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600">
+              onClick={() => loginSubmit}
+              className="flex w-full justify-center rounded-md bg-indigo-600 px-3 py-1.5 text-sm font-semibold leading-6 text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
+            >
               Sign in
             </button>
           </div>
@@ -65,7 +94,8 @@ export default function Login() {
           Not a member?
           <a
             href="/register"
-            className="font-semibold leading-6 text-indigo-600 hover:text-indigo-500">
+            className="font-semibold leading-6 text-indigo-600 hover:text-indigo-500"
+          >
             Create you own account
           </a>
         </p>

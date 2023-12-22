f2341c9287812e51e14fe5a357fcd3550ccc8740
HaithamBH <haitham.benhammouda@gmail.com>
2023-12-22 22:31:16 +0100

    Add public layout component for protected routes

---
+++ src/layout/publicLayout.tsx
@@ -0,0 +1,17 @@
+import { Outlet, useNavigate } from "react-router-dom";
+import { useAuthStore } from "../store/auth";
+import { useEffect } from "react";
+
+const ProtectedLayout = () => {
+  const { isAuth } = useAuthStore();
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    if (isAuth) {
+      navigate("/chat");
+    }
+  });
+
+  return <Outlet />;
+};
+export default ProtectedLayout;

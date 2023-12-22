9d8259d799f69996f0a13d92145506e2e7e45192
HaithamBH <haitham.benhammouda@gmail.com>
2023-12-22 22:31:01 +0100

    Refactor auth store to include token and add checkToken method

--- src/store/auth.ts
+++ src/store/auth.ts
@@ -2,28 +2,28 @@
 import { create } from "zustand";
 import api from "./api";
 import { createJSONStorage, persist } from "zustand/middleware";
-import { connectSocket } from "../websocket/init";
 
 interface IAuth {
   username: string;
   isAuth: boolean;
+  token: string;
   register: (username: string, email: string, password: string) => void;
   login: (username: string, password: string) => void;
+  checkToken: () => void;
 }
 
 export const useAuthStore = create(
   persist<IAuth>(
-    (set) => ({
+    (set, get) => ({
       username: "",
       isAuth: false,
+      token: "",
       register: (username: string, email: string, password: string) => {
         api
           .post("/register", { username, email, password })
           .then((response: any) => {
-            localStorage.setItem("token", response.data.token);
-            set({ isAuth: true });
-            connectSocket();
-            window.location.href = "/chat";
+            set({ isAuth: true, token: response.data.token });
+            window.location.href = "/";
           })
           .catch((error) => {
             // TODO: catch error
@@ -32,23 +32,35 @@ export const useAuthStore = create(
           });
       },
 
-      login: (username: string, password: string) => {
+      login: (email: string, password: string) => {
         api
-          .post("/login", { username, password })
-          .then((data: any) => {
-            localStorage.setItem("token", data.token);
-            set({ isAuth: true });
-            connectSocket();
-            window.location.href = "/";
+          .post("/login", { email, password })
+          .then((response: any) => {
+            set({ isAuth: true, token: response.data.token });
+            window.location.href = "/chat";
           })
           .catch((error) => {
             console.log(error);
           });
       },
+
+      checkToken: () => {
+        const { token } = get();
+        api
+          .get("checkToken", { params: { token } })
+          .then((response: any) => {
+            set({ isAuth: true, username: response.data.username });
+          })
+          .catch((error) => {
+            if (error.status === 401) {
+              set({ token: "" });
+            }
+          });
+      },
     }),
     {
       name: "auth-storage",
-      storage: createJSONStorage(() => sessionStorage),
+      storage: createJSONStorage(() => localStorage),
     },
   ),
 );

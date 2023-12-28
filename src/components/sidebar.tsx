43347df26be0247bacb3226c0976c75f7288b403
HaithamBH <haitham.benhammouda@gmail.com>
2023-12-28 20:34:50 +0100

    Refactor sidebar component to fetch and display friend list

--- src/components/sidebar.tsx
+++ src/components/sidebar.tsx
@@ -1,14 +1,29 @@
-import Friend from './friend';
+import { useEffect } from "react";
+import { useProfileStore } from "../store/profile";
+import Friend from "./friend";
+import { useNavigate } from "react-router-dom";
 
 const Sidebar = () => {
+  const { fetchFriendList, friends } = useProfileStore();
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    if (!friends) {
+      fetchFriendList();
+    } else if (friends.length > 0) {
+      console.log(friends);
+      navigate(`/chat/${friends[0]._id}`);
+    }
+  }, [friends]);
   return (
     <div className="h-[calc(100vh-4rem)] w-60 fixed bg-base-100">
-      <Friend
-        name={'Haithem BenHammouda'}
-        lastMsg={'test last message aaaaaaa'}
-        profilePhoto={'https://www.shareicon.net/data/256x256/2016/05/26/771188_man_512x512.png'}
-        isOnline={false}
-      />
+      {friends.map((friend) => (
+        <Friend
+          name={friend.name}
+          profilePhoto={"https://www.shareicon.net/data/256x256/2016/05/26/771188_man_512x512.png"}
+          isOnline={false}
+        />
+      ))}
     </div>
   );
 };

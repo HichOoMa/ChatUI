50f9f6b9e89820fa2f5d1b79a88df1b2d14d80af
HaithamBH <haitham.benhammouda@gmail.com>
2023-12-29 20:23:15 +0100

    Refactor Messages component to display messages based on friend id

--- src/pages/messages.tsx
+++ src/pages/messages.tsx
@@ -1,25 +1,40 @@
-import Message from '../components/message';
-import SendField from '../components/sendField';
+import { useEffect } from "react";
+import Message from "../components/message";
+import SendField from "../components/sendField";
+import { useProfileStore } from "../store/profile";
+import { useParams } from "react-router-dom";
+import { useAuthStore } from "../store/auth";
 
 const Messages = () => {
-  const d = new Date();
+  const { username } = useAuthStore();
+  const { messages, friends, fetchMessages } = useProfileStore();
+  // friend id
+  const { id } = useParams();
+  const friend = friends && friends.find((friend) => friend._id === id);
+  if (id && !messages[id]) {
+    fetchMessages(id);
+  }
+  useEffect(() => {}, [messages]);
   return (
     <main className="h-full w-full relative">
       <div className="p-4">
-        <Message
-          username={'Hamed'}
-          userphotoUrl={'https://www.shareicon.net/data/512x512/2016/09/15/829452_user_512x512.png'}
-          content={'ooooooooo allaowska test test test'}
-          date={d}
-          showDetails={false}
-        />
-        <Message
-          username={'Hamed'}
-          userphotoUrl={'https://www.shareicon.net/data/512x512/2016/09/15/829452_user_512x512.png'}
-          content={'ooooooooo allaowska test test test'}
-          date={d}
-          showDetails={true}
-        />
+        {id &&
+          messages[id] &&
+          messages[id]?.map((message) => {
+            return (
+              <Message
+                key={message._id}
+                username={message.sender_id === id && friend ? friend.name : username}
+                userphotoUrl={
+                  "https://www.shareicon.net/data/512x512/2016/09/15/829452_user_512x512.png"
+                }
+                content={message.content}
+                date={new Date(Date.parse(message.date))}
+                showDetails={false}
+                userSender={message.sender_id !== id}
+              />
+            );
+          })}
       </div>
       <SendField />
     </main>
